2ef8337ff1f1c3ba60c85c58427331619d05403f
Chingthangkhomba <bheigachandrameetei@gmail.com>
2025-02-28 13:40:19 +0530

    add allouwed cors origins

--- tamravarsity/settings.py
+++ tamravarsity/settings.py
@@ -62,7 +62,8 @@ MIDDLEWARE = [
 
 # Allow your frontend to access the API
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # React frontend
+    "http://localhost:5173",
+    "https://tamraversity-backend.lamzingtech.com",  # React frontend
 ]
 # If you want to allow all origins (not recommended for production)
 CORS_ALLOW_ALL_ORIGINS = True
